fd292cd4b7d15bc47d1c8a1644bc338612fa95b0
jks3891 <jks3891@naver.com>
2024-11-07 15:43:01 +0900

    공통 js 코드 수정

--- docs/script/script.js
+++ docs/script/script.js
@@ -47,46 +47,56 @@ $(".icon-menu").click(function () {
 
 // 모바일 메뉴 구현
 function MobileMenuBox1__init() {
-  // $('.mobile-menu-box-1 ul').slideUp();
+  // a 태그 클릭 이벤트
+  $(".mobile-menu-box ul li > a").click(function (e) {
+    let $this = $(this).parent(); // 클릭된 a 태그의 부모 li 요소
+    let hasSubmenu = $this.find("> ul").length > 0; // 하위 메뉴 존재 여부 확인
+    let href = $(this).attr("href"); // a 태그의 href 값 가져오기
 
-  $(".mobile-menu-box ul > li").click(function () {
-    let $this = $(this);
-    let has = $this.hasClass("active");
-
-    // 클릭당한 대상의 active 붙어있는 형제들 먼저 호출
-    // 그의 모든 후손은 slideUp이 되라!!
-    $this.siblings(".active").find("ul").stop().slideUp(300);
-
-    // 클릭당한 대상의 active 붙어있는 형제들 먼저 호출
-    // 형제들 호출 뒤, active 붙어있는 후손들부터 active 제거!
-    $this.siblings(".active").find(".active").removeClass("active");
-
-    // 클릭당한 대상의 active 붙어있는 형제들 호출 후 active 제거!
-    $this.siblings(".active").removeClass("active");
-
-    if (has) {
-      $this.find("> ul").stop().slideUp(300);
-      $this.find(".active").removeClass("active");
-      $this.removeClass("active");
-    } else {
-      $this.find("> ul").stop().slideDown(300);
-      $this.addClass("active");
+    // 하위 메뉴가 있고 href가 "#"인 경우 기본 동작을 막고 서브메뉴 토글
+    if (hasSubmenu && href === "#") {
+      e.preventDefault(); // 기본 동작 막기
+      toggleSubMenu($this); // 서브메뉴 토글 함수 호출
     }
-
-    // mobile-menu-box의 하위 li 중 active가 있는지 확인
-    if ($(".mobile-menu-box ul > li.active").length > 0) {
-      // active가 있으면 banner에 active 추가
-      $(".mobile-side-bar__banner").addClass("active");
-    } else {
-      // active가 없으면 banner에서 active 제거
-      $(".mobile-side-bar__banner").removeClass("active");
+    // 하위 메뉴가 없고 실제 URL인 경우 링크 이동 허용
+    else if (!hasSubmenu && href !== "#") {
+      window.location.href = href; // 링크 이동
     }
   });
 
+  // 서브메뉴 토글 함수 정의
+  function toggleSubMenu($menuItem) {
+    let isActive = $menuItem.hasClass("active");
+
+    // 다른 활성화된 형제들 초기화
+    $menuItem.siblings(".active").find("ul").stop().slideUp(300);
+    $menuItem.siblings(".active").find(".active").removeClass("active");
+    $menuItem.siblings(".active").removeClass("active");
+
+    // 현재 항목 토글
+    if (isActive) {
+      $menuItem.find("> ul").stop().slideUp(300);
+      $menuItem.find(".active").removeClass("active");
+      $menuItem.removeClass("active");
+    } else {
+      $menuItem.find("> ul").stop().slideDown(300);
+      $menuItem.addClass("active");
+    }
+
+    // 하위 li 중 active가 있는지 확인하여 배너에 active 클래스 추가/제거
+    if ($(".mobile-menu-box ul > li.active").length > 0) {
+      $(".mobile-side-bar__banner").addClass("active");
+    } else {
+      $(".mobile-side-bar__banner").removeClass("active");
+    }
+  }
+
+  // 이중 클릭 방지
   $(".mobile-menu-box ul").click(function () {
     return false;
   });
 
+  // 메뉴 초기화
   $(".icon-menu .line").click(function () {
     $(".mobile-menu-box ul li ul").stop().slideUp(300);
     $(".mobile-side-bar__banner").removeClass("active");
